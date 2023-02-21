dd318a6f32aa031fb473fe05e3331d36877e7de1
Jorge Pina Yus <113890429+jorgepinayus@users.noreply.github.com>
2023-02-21 10:14:10 +0100

    Rename inputoutput.cpp to 04.Entrada y salida de datos.cpp

--- 04.Entrada y salida de datos.cpp
+++ 04.Entrada y salida de datos.cpp
@@ -1,8 +1,8 @@
-//Instrucciones básicas
+//Instrucciones bÃ¡sicas
 #include <iostream>
 using namespace std;
 int main(){
-	//definición de 3 variables
+	//definiciÃ³n de 3 variables
 	int x , y , z;
 	cout<<"introdice 3 variables. ";
 	cin>> x >> y >> z; 
